2114e8bc17669e88f9e0fcdb244d11e5118b1362
MAHENDRA REDDY <70009808+mahiprime2001@users.noreply.github.com>
2025-06-13 10:18:24 +0530

    Fix Sass circular import error

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "./src/styles/globals.scss";`,
-      },
-    },
-  },
   optimizeDeps: {
     exclude: ['@mui/material', '@mui/icons-material'],
   },
