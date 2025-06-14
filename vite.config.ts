61761122319bccac140ee72aeee8f7b714d3d055
MAHENDRA REDDY <70009808+mahiprime2001@users.noreply.github.com>
2025-06-14 10:48:40 +0530

    Fix react-is ForwardRef export error

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['prop-types'],
+    include: ['prop-types', 'react-is'],
     exclude: ['@mui/icons-material'],
   },
 });
